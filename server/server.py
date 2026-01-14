6d630027695aa31feeaf6ca6da9fd3474e988431
seokmin100 <137677378+seokmin100@users.noreply.github.com>
2026-01-14 14:56:46 +0900

    server 6

--- server/server.py
+++ server/server.py
@@ -6,24 +6,22 @@ from faster_whisper import WhisperModel
 model = WhisperModel(
     "base",
     device="cpu",
-    compute_type="int8"  # CPU 필수
+    compute_type="int8"
 )
 
 SAMPLE_RATE = 16000
-BUFFER_SECONDS = 2
+BUFFER_SECONDS = 3
 BUFFER_SIZE = SAMPLE_RATE * BUFFER_SECONDS
 
-audio_buffer = np.array([], dtype=np.float32)
-
 async def handler(ws):
-    global audio_buffer
+    audio_buffer = np.array([], dtype=np.float32)
     print("Client connected")
 
     async for message in ws:
         if not isinstance(message, bytes):
             continue
 
-        # ⭐ Int16 PCM → float32
+        # Int16 PCM → float32
         chunk = np.frombuffer(message, dtype=np.int16).astype(np.float32) / 32768.0
         audio_buffer = np.concatenate([audio_buffer, chunk])
 
@@ -44,9 +42,11 @@ async def handler(ws):
             )
 
             text = "".join(seg.text for seg in segments).strip()
-            if text:
-                #print("STT:", text)
-                await ws.send(text)
+
+            if len(text.replace(" ", "")) < 3:
+                continue
+
+            await ws.send(text)
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", 3000, max_size=None):
